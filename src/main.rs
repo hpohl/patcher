c63df2931d3b5ca152246549312e961f75322496
Henning Pohl <henning@still-hidden.de>
2017-12-09 23:22:32 +0100

    listen

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 
 
 fn main() {
-    ws::listen(("127.0.0.1", env::var("PORT").unwrap().parse::<u16>().unwrap()), |out| {
+    ws::listen(("0.0.0.0", env::var("PORT").unwrap().parse::<u16>().unwrap()), |out| {
         move |msg| {
             println!("{}", msg);
             out.send(msg)
